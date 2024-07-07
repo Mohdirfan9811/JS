45488eaeb6f11129e509cc12f76f84346a573c41
Mohdirfan9811 <160612139+Mohdirfan9811@users.noreply.github.com>
2024-07-07 07:18:50 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 1424242
+let accountEmail = "mohdirfa2412@gmail.com"
+var accountPassword = "12424"
+accountCity = "New Delhi"
+
+ //accountId = 5
+ accountEmail = "irfan.com"
+ accountPassword = "1212"
+ accountCity = "kolkata"
+
+console.log("accountId");
+console.table([accountId, accountEmail, accountPassword, accountCity])
