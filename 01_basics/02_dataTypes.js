849d4de9b4bf0bc444900be2c4b41a84e852e3f3
Mohdirfan9811 <160612139+Mohdirfan9811@users.noreply.github.com>
2024-07-08 14:31:12 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Mohd Irfan")
+
+// Data Types
+
+//Number => 2 to power 53
+//bigint
+//string => as wish use "" , ''
+// boolean => true/false
+// Null => standalone value
+// undefined =>
+//sumbol => unique
+
+//object
+
